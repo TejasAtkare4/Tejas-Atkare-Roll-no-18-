3d6b776b8f23c86f138a836b2f32aa61b83b7a77
Tejas Atkare <tejasatkare4@gmail.com>
2026-04-27 13:49:17 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class add{
+    public :
+        add(int a , int b ){
+            int c = a +b ;
+            cout<<"sum = "<<c ;
+
+        }
+};
+int main(){
+    add object(10,20);
+}

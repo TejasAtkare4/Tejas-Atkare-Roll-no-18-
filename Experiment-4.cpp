ffb5526a7a79b784c5e0f8294cbae39c143114e4
Tejas Atkare <tejasatkare4@gmail.com>
2026-04-27 13:50:11 +0530

    Add files via upload

---
+++ Experiment-4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class myclass{
+    public : 
+        void mymethod(){
+            cout<<"HELLOW";
+        }
+};
+int main(){
+    myclass a1 ;
+    a1.mymethod() ;
+
+}
